61e5b70534c8b90d8da4fa787f3a2b9fcbd6960e
jafnathajudheen <jafnathajudheen0@gmail.com>
2025-10-04 12:30:20 +0530

    ss

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+require ('dotenv').config();
 const express = require('express');
-const { resolve } = require('path');
+const mongoose=require('mongoose');
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(express.json());
+const MONGO_URI='mongodb://localhost:27017/eventsDB';
+const PORT=process.env.port||3000
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+mongoose.connect(MONGO_URI,{
+  useUnifiedTopology:true,
+  useNewUrlParser:true
+})
+.then(()=>{
+  console.log("Connected to MongoDB");
+})
+.catch((err)=>{
+  console.log(`Error connecting to MongoDB${err}`);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server at http://localhost:${PORT}`);
 });
